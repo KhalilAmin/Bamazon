aabee1e56208c347819638c767503bc51d2c7bb8
khalil <kamin86@gmail.com>
2018-03-15 01:43:43 -0400

    final

--- Bamazon.sql
+++ Bamazon.sql
@@ -4,7 +4,7 @@ CREATE DATABASE bamazon;
 USE bamazon;
 
 CREATE TABLE products (
-  item_id INT NOT NULL,
+  item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(100) NULL,
   department_name VARCHAR(100) NULL,
   price DECIMAL (10,2) NULL,
@@ -13,34 +13,34 @@ CREATE TABLE products (
 );
 
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUE (0001, "Chair", "Furniture", 49.99, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Chair", "Furniture", 49.99, 100);
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUE (0002, "Table", "Furniture", 249.99, 25);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Table", "Furniture", 249.99, 25);
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUE (0003, "Desk", "Furniture", 199.99, 30);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Desk", "Furniture", 199.99, 30);
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUE (0004, "Laptop", "Electronics", 1199.99, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Laptop", "Electronics", 1199.99, 20);
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUE (0005, "Television", "Electronics", 499.99, 50);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Television", "Electronics", 499.99, 50);
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUE (0006, "Lawnmower", "Garden", 149.99, 10);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Lawnmower", "Garden", 149.99, 10);
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUE (0007, "Headphones", "Electronics", 19.99, 50);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Headphones", "Electronics", 19.99, 50);
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUE (0008, "Printer", "Electronics", 99.99, 40);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Printer", "Electronics", 99.99, 40);
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUE (0009, "Gas Mask", "Outdoor", 49.99, 15);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Gas Mask", "Outdoor", 49.99, 15);
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUE (0010, "Walkie-Talkie", "Electronics", 19.99, 25);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Walkie-Talkie", "Electronics", 19.99, 25);
 
-SELECT item_id, product_name, department_name, price, stock_quantity FROM products;
+SELECT product_name, department_name, price, stock_quantity FROM products;
